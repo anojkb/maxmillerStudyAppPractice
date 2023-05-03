49865bfc259ed28560520efbdc6f9d86cb149245
anojkb <anojkb.ui@gmail.com>
2023-05-03 18:37:20 +0530

    data bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'maxmaxmillerStudyAppPractice';
+  serverElements = [{type: 'server',name: 'TestServer', content:'just a test!' }];
 }
